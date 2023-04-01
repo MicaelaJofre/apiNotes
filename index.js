8432c1105c04a9f0ef625efad5b1bc67cb0a0124
Micaela Analia Jofre <mkjofre@gmail.com>
2023-04-01 00:51:33 -0300

    Added cors and implements middleware

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+const { logger, unknownEndpoint } = require('./loggerMiddleware')
 
+app.use(cors())
 app.use(express.json())
+app.use(logger)
 
 let notes = [
   {
@@ -25,42 +29,48 @@ let notes = [
 ]
 
 app.get('/api/notes', (req, resp) => {
-  resp.json(notes)
+  resp.status(200).json(notes)
 })
 
 app.get('/api/notes/:id', (req, resp) => {
   const id = Number(req.params.id)
-  const note = notes.find(note => note.id === id)
-  resp.json(note)
+  const note = notes.find((note) => note.id === id)
+  resp.status(200).json(note)
 })
 
 app.delete('/api/notes/:id', (req, resp) => {
   const id = Number(req.params.id)
-  notes = notes.filter(note => note.id !== id)
+  notes = notes.filter((note) => note.id !== id)
 
   resp.status(204).end()
 })
 
-app.post('/api/notes',(req,resp)=>{
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0
+  return maxId + 1
+}
+
+app.post('/api/notes', (req, resp) => {
   const body = req.body
 
-  if(!body.content){
-    return response.status(400).json({ 
-      error: 'content missing' 
+  if (!body.content) {
+    return resp.status(400).json({
+      error: 'content missing'
     })
   }
-
   const newNote = {
-    id: notes.length + 1,
+    id: generateId(),
     content: body.content,
     date: new Date(),
-    completed: body.completed || false 
+    completed: body.completed || false
   }
-  
+
   notes = notes.concat(newNote)
 
   resp.status(200).json(notes)
 })
+
+app.use(unknownEndpoint)
 
 const PORT = '3001'
 app.listen(PORT, () => console.log(`El servidor se abrirá por el puerto ${PORT}`))
