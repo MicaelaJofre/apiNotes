3ed70241fe92d4dfff771880638ac2c759a59669
Micaela Analia Jofre <mkjofre@gmail.com>
2023-05-09 20:16:41 -0300

    Defined models notes

---
+++ models/note.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const noteSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  date: Date,
+  important: Boolean
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Note', noteSchema)
